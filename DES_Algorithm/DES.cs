c4ac0cd9390774256e9a3a8c2263156f821f8fb5
eronek345 <eronek345@gmail.com>
2019-04-13 11:07:14 +0200

    Dodaj pliki projektów.

---
+++ DES_Algorithm/DES.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DES_Algorithm
+{
+    class DES
+    {
+        BitArray bitArray = new BitArray(64);
+
+        public DES(BitArray array)
+        {
+            this.bitArray = array;
+        }
+
+        public void DES_Cipher()
+        {
+            InitialPermutation();
+        }
+
+        private void InitialPermutation()
+        {
+
+        }
+        private void InverseInitialPermutation()
+        {
+
+        }
+    }
+}
